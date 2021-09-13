795fd0788c6169f51fe411f5737f41cabb044b28
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-09-13 17:52:59 +0100

    Commit for Tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-600 flex flex-col justify-center items-center h-screen">
+      <h1 className="text-white text-3xl font-semibold">Hello World</h1>
+
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-green-500 text-white shadow-xl hover:bg-green-700 p-3 rounded-sm flex"
+      >
+        Increment
+      </button>
+
+      <p className="text-white text-9xl">{count}</p>
     </div>
   );
 }
